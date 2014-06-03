73a2917564ac9b03328b2eb610d2d0bad5cef1a6
Guillem Barba <guillem@nan-tic.com>
2014-06-03 19:16:21 +0200

    Add COPYRIGHT advice and formatting

--- opportunity.py
+++ opportunity.py
@@ -1,13 +1,10 @@
-#This file is part of sale_opportunity_campaign module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
 from trytond.model import ModelView, ModelSQL, fields
-from trytond.pool import PoolMeta, Pool
-
-__metaclass__ = PoolMeta
+from trytond.pool import PoolMeta
 
 __all__ = ['SaleOpportunity', 'SaleOpportunityCampaign']
+__metaclass__ = PoolMeta
 
 
 class SaleOpportunityCampaign(ModelSQL, ModelView):
